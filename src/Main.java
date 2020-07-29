9dbbaaf3a912f5db7776a9a881d6585f4f2dc428
Łukasz Szczurowski <lszczurowski0@gmail.com>
2020-07-29 10:09:45 +0200

    add method checkPalindrom(int[] table)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] tab1 = {1, 2, 3, 4, 5};
+        int[] tab2 = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+
+        System.out.println("Tablica: " + Arrays.toString(tab1) + " ,jest palindromem: " + checkPalindrom(tab1));
+        System.out.println("Tablica: " + Arrays.toString(tab2) + " ,jest palindromem: " + checkPalindrom(tab2));
+    }
+
+    private static boolean checkPalindrom(int[] table) {
+        for (int i = 0; i < (table.length / 2); i++) {
+            if (table[i] == table[table.length - 1 - i])
+                return true;
+        }
+        return false;
     }
 }
