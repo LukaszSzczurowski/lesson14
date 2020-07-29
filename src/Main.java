a20481b5ec08c451049ae35c4b1962d3051d4c66
Łukasz Szczurowski <lszczurowski0@gmail.com>
2020-07-29 10:48:13 +0200

    create algorytm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Witaj w programie w którym wyświetlę Ci sume wszystkich podanych przez Ciebie cyfr");
+
+        int x = 0;
+        int sum = 0;
+
+        System.out.println("Podaj cyfrę: ");
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        while (Math.pow(number = scanner.nextInt(), 2) < 100) {
+            sum += number;
+        }
+
+        if (sum % 2 == 0) {
+            System.out.println("Suma twoich liczb jest parzysta");
+        }
+        else {
+            System.out.println("Suma twoich liczb jest nieparzysta");
+        }
+
+        System.out.println("Suma twoich liczb wymosi: " + sum);
     }
 }
